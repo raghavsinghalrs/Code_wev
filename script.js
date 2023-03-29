9bd2b64f870cd8ea0d290272336ecaad9478d454
raghavsinghalrs <22sep2001.rs@gmail.com>
2023-03-29 17:21:29 +0530

    Add delete and Add functionality

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
-console.log("Hello World");
+var add = document.getElementById('addforms');
+var item_list = document.getElementById('lists');
+var filter = document.getElementById('filter');
+
+add.addEventListener('submit', appenditem);
+item_list.addEventListener('click',removeitem);
+filter.addEventListener('keyup',filteritem);
+function removeitem(x){
+    if(x.target.id==='del'){
+        if(confirm('Are you sure?')){
+            var li = x.target.parentElement;
+            item_list.removeChild(li);
+        }
+    }
+}
+    
+function appenditem(e){
+    e.preventDefault();
+    var value = document.getElementById('tasks').value;
+    var li = document.createElement('li');
+    li.id = 'task';
+    var input = document.createElement("input");
+    input.type = 'text';
+    input.value=value;
+    li.appendChild(input);
+    var del = document.createElement('button');
+    del.id='del';
+    del.textContent='del'
+    li.appendChild(del);
+    item_list.appendChild(li);
+}
+
+function filteritem(e){
+    var text = e.target.value
+}
